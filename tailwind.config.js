17dc3ca9c589f89878373719dbf1811b99a79c92
LeBrontoJames <keenanabigailidn@gmail.com>
2025-02-19 08:23:54 +0700

    New Version Soon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html", // ✅ Include index.html at root
+    "./**/*.{html,js}", // ✅ Scan all HTML & JS files inside subdirectories
+  ],
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
         dmsans: ['DM Sans', 'sans-serif'],
-      }
+      },
     },
   },
   plugins: [],
-}
-
+};
